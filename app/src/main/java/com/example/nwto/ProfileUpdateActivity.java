7a655cc65e9430313e867c5244c11afadf53829b
unknown <heatherwang39@gmail.com>
2021-02-24 11:17:02 -0500

    Add ProfileUpdate layout.

---
+++ app/src/main/java/com/example/nwto/ProfileUpdateActivity.java
@@ -0,0 +1,14 @@
+package com.example.nwto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileUpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_update);
+    }
+}
