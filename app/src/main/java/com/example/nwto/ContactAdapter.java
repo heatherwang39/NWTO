d4abdae3ef1206f066e2b7f92917f5cc0d9523c2
unknown <heatherwang39@gmail.com>
2021-03-05 22:51:00 -0500

    Allow User to delete a contact.

--- app/src/main/java/com/example/nwto/ContactAdapter.java
+++ app/src/main/java/com/example/nwto/ContactAdapter.java
@@ -1,13 +1,24 @@
 package com.example.nwto;
 
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.List;
 
@@ -16,11 +27,15 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHold
     List<Contact> contactList;
     LayoutInflater inflater;
     Context ctx;
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
 
     public ContactAdapter(Context ctx, List<Contact> contactList){
         this.ctx = ctx;
         this.contactList = contactList;
         this.inflater = LayoutInflater.from(ctx);
+        // Initialize Cloud FireStore
+        db = FirebaseFirestore.getInstance();
     }
 
     @NonNull
@@ -36,6 +51,31 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHold
         holder.textFullName.setText(contactList.get(position).getFullName());
         holder.textEmail.setText(contactList.get(position).getEmail());
         holder.textPhoneNumber.setText(contactList.get(position).getPhoneNumber());
+        holder.imageDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //delete the contact
+                db.collection("contacts")
+                        .whereEqualTo("email",contactList.get(position).getEmail())
+                        .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (DocumentSnapshot document : task.getResult()) {
+                                db.collection("contacts").document(document.getId()).delete();
+                                Toast.makeText(ctx, "Contact deleted.", Toast.LENGTH_SHORT).show();
+                                Log.d("Delete Contact", "Successfully deleting contact document: "+document.getId());
+                                contactList.remove(position);
+                                ctx.startActivity(new Intent(ctx, NeighboursActivity.class));
+                            }
+                        } else {
+                            Log.d("Delete Contact", "Error getting comment documents when deleting: ", task.getException());
+                        }
+                    }
+                });
+            }
+        });
+
 //        holder.postView.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
@@ -57,12 +97,16 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHold
         TextView textFullName;
         TextView textEmail;
         TextView textPhoneNumber;
+        ImageView imageEdit;
+        ImageView imageDelete;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             textFullName = itemView.findViewById(R.id.text_full_name);
             textEmail = itemView.findViewById(R.id.text_email);
             textPhoneNumber = itemView.findViewById(R.id.text_phone_number);
+            imageEdit = itemView.findViewById(R.id.image_edit);
+            imageDelete = itemView.findViewById(R.id.image_delete);
         }
     }
 
