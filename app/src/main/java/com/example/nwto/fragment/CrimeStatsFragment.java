e6c83eab7094880b5aef420ee137e7e515cb5ebc
dankunlee <45495755+dankunlee@users.noreply.github.com>
2021-03-27 00:51:24 -0400

    Uncomment code for graph label space

--- app/src/main/java/com/example/nwto/fragment/CrimeStatsFragment.java
+++ app/src/main/java/com/example/nwto/fragment/CrimeStatsFragment.java
@@ -125,7 +125,7 @@ public class CrimeStatsFragment extends Fragment {
 
         // sets renderers settings
         gridRenderer.setLabelFormatter(staticLabelsFormatter);
-//        gridRenderer.setLabelsSpace(20);
+        gridRenderer.setLabelsSpace(20);
         gridRenderer.setHorizontalLabelsAngle(110);
         gridRenderer.setHighlightZeroLines(false);
         viewport.setMinX(-1);
