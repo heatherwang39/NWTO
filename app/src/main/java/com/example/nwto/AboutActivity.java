89625893d46f02ab98c6d0beee671fb9a7ef14c9
dankunlee <45495755+dankunlee@users.noreply.github.com>
2021-03-27 21:17:32 -0400

    Fill About NWTO page

--- app/src/main/java/com/example/nwto/AboutActivity.java
+++ app/src/main/java/com/example/nwto/AboutActivity.java
@@ -1,15 +1,24 @@
 package com.example.nwto;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutActivity extends AppCompatActivity {
 
+    private TextView mContent;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        mContent = (TextView) findViewById(R.id.about_NWTO);
+        mContent.setMovementMethod(LinkMovementMethod.getInstance());
+        mContent.setText(Html.fromHtml(getString(R.string.aboutNWTO)));
     }
 }
