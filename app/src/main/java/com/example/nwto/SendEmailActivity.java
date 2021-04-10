d3dab308e1f4ff967e6824ef2bf44839488e6b5e
Heather Wang <heatherwang39@gmail.com>
2021-04-10 16:32:37 -0400

    Allow User to select picture from gallery when sending email to neighbours.

--- app/src/main/java/com/example/nwto/SendEmailActivity.java
+++ app/src/main/java/com/example/nwto/SendEmailActivity.java
@@ -1,11 +1,15 @@
 package com.example.nwto;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.FileProvider;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
@@ -33,13 +37,14 @@ public class SendEmailActivity extends AppCompatActivity {
 
     private static final String TAG = "Send Email";
     private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_IMAGE_SELECT = 2;
 
     private String mOwnerUID, mSubject, mBody;
     private ArrayList<String> mEmailList;
     private ArrayList<Neighbour> mNeighbourList = NeighboursActivity.mNeighbourList;
     private EditText mEditSubject, mEditBody;
     private Button mButtonCancel, mButtonSend;
-    private TextView mTextTakeAPicture, mTextPreview;
+    private TextView mTextTakePicture, mTextSelectFromGallery, mTextPreview;
     private ImageView mImagePreview;
     private Uri mAttachmentUri;
     private Bitmap mImageBitmap;
@@ -77,11 +82,23 @@ public class SendEmailActivity extends AppCompatActivity {
             }
         });
 
-        mTextTakeAPicture = findViewById(R.id.text_take_a_picture);
-        mTextTakeAPicture.setOnClickListener(new View.OnClickListener() {
+        mTextTakePicture = findViewById(R.id.text_take_picture);
+        mTextTakePicture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 takePicture();
+            }
+        });
+
+        mTextSelectFromGallery = (TextView) findViewById(R.id.text_select_from_gallery);
+        mTextSelectFromGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(SendEmailActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(SendEmailActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_IMAGE_SELECT);
+                } else {
+                    selectFromGallery();
+                }
             }
         });
 
@@ -128,22 +145,6 @@ public class SendEmailActivity extends AppCompatActivity {
                     Toast.LENGTH_SHORT).show();
         }
 
-
-//        Intent email = new Intent(Intent.ACTION_SENDTO);
-//        email.setData(Uri.parse("mailto:"));
-//        email.putExtra(Intent.EXTRA_EMAIL, emails);
-//        email.putExtra(Intent.EXTRA_SUBJECT, mSubject);
-//        email.putExtra(Intent.EXTRA_TEXT, mBody);
-
-//        Log.d(TAG, "all emails:" + emails);
-//
-//        if (email.resolveActivity(getPackageManager()) != null) {
-//            startActivity(email);
-//        } else {
-//            Toast.makeText(SendEmailActivity.this, "There is no application that support this email action",
-//                    Toast.LENGTH_SHORT).show();
-//        }
-
     }
 
     private void takePicture() {
@@ -165,6 +166,23 @@ public class SendEmailActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_IMAGE_SELECT) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                selectFromGallery();
+            }
+        }
+    }
+
+    private void selectFromGallery() {
+        // Pick images from Gallery
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/*");
+        startActivityForResult(intent, REQUEST_IMAGE_SELECT);
+    }
+
+    @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
@@ -179,9 +197,21 @@ public class SendEmailActivity extends AppCompatActivity {
                 e.printStackTrace();
                 Log.i(TAG, "onActivityResult ok: get postBitmap unsuccessfully");
             }
-        } else if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_CANCELED) {
+        } else if (requestCode == REQUEST_IMAGE_SELECT && resultCode == Activity.RESULT_OK && data != null) {
+            mAttachmentUri = data.getData();
+            try {
+                mImageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), mAttachmentUri);
+                mImagePreview.setVisibility(View.VISIBLE);
+                mImagePreview.setImageBitmap(mImageBitmap);
+                mTextPreview.setVisibility(View.VISIBLE);
+                Log.i(TAG, "onActivityResult ok: get postBitmap successfully");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if ((requestCode == REQUEST_IMAGE_CAPTURE || requestCode == REQUEST_IMAGE_SELECT) && resultCode == Activity.RESULT_CANCELED) {
             Log.i(TAG, "onActivityResult: Make Post Image Capture RESULT CANCELLED");
         } else {
+
         }
     }
 
